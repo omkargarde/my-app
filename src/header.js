43be97068c1d0b0f6dae6bee558565fee8c9b9cc
omkargarde <omyagarde@gmail.com>
2022-09-14 07:49:43 +0530

    main: day 3
    learnt react custom component and solved css import issue using node scss

---
+++ src/header.js
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar">
+                <div className="navbar-header">header is not working</div>
+                <ul className="navbar-items">
+                    <li>
+                        contact
+                    </li>
+                    <li>
+                        price
+                    </li>
+                    <li>
+                        about
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-const navbar = (
-  <main>
-    <h1>react</h1>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  </main>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.scss';
+import Header from "./header";
+import Footer from "./footer";
+import MainContent from "./mainContent";
+function Page() {
+  return (
+    <div>
+      <Header></Header>
+      <MainContent></MainContent>
+      <Footer></Footer>
+    </div>
+  )
+}
 
 // ReactDOM.render(navbar, document.getElementById("root"))
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(navbar)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Page />);
