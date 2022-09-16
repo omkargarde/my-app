43f84e81c4dc366b5422f8a4909137282420ae44
omkargarde <omyagarde@gmail.com>
2022-09-16 07:01:19 +0530

    main: day 4
    added file structure

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Header from "./components/header/header";
+import MainContent from "./components/mainContent/mainContent";
+
+export default function App() {
+    return (
+        <div>
+            <Header></Header>
+            <MainContent></MainContent>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.scss';
-import Header from "./header";
-import Footer from "./footer";
-import MainContent from "./mainContent";
-function Page() {
-  return (
-    <div>
-      <Header></Header>
-      <MainContent></MainContent>
-      <Footer></Footer>
-    </div>
-  )
-}
+import App from "./app";
 
-// ReactDOM.render(navbar, document.getElementById("root"))
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Page />);
+root.render(<App />);
